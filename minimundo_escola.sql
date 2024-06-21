da15384cff0fde8f123df30c0796c3bf23893f35
LckzUp <160252761+LckzUp@users.noreply.github.com>
2024-06-21 12:00:39 -0300

    Add files via upload

---
+++ minimundo_escola.sql
@@ -0,0 +1,20 @@
+create database escola;
+
+use escola;
+
+create table professor(
+	nome varchar(255),
+    cpf varchar(45),
+    pk int
+);
+
+create table sala(
+	pk int,
+    numero_sala varchar(4),
+    qtd_aluno int
+);
+
+create table turma(
+	pk int,
+    nome_turma varchar(45)
+);
